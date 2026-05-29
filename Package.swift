6c13a0510027c2adbd762d6332cbc6edb51e7119
Stafox <staskuryan@gmail.com>
2026-05-29 15:04:58 +0200

    Release 0.5.2

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 
 // MARK: - Release Configuration
 // Updated automatically by CI when publishing a new release
-let version = "0.5.1"
-let checksum = "1b5618b61e3b78fd321ff06976102d243898bbbfa0705a37e99bc6353e87057b"
+let version = "0.5.2"
+let checksum = "2cf2c3781174e567e2c01e974d4832292d28abe2cf2d49f937f2ede98f25a752"
 
 let package = Package(
     name: "AmplySDK",
